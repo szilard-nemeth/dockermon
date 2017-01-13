7c8994ed1d79914df261debbedb1a772bd3ad65d
szyszy <nsziszy@gmail.com>
2017-01-13 09:29:28 +0100

    added restart dead/unhealthy containers functionality

--- dockermon.py
+++ dockermon.py
@@ -11,6 +11,7 @@ import shlex
 
 if version_info[:2] < (3, 0):
     from httplib import OK as HTTP_OK
+    from httplib import NO_CONTENT as HTTP_NO_CONTENT
     from urlparse import urlparse
 else:
     from http.client import OK as HTTP_OK
@@ -68,7 +69,7 @@ def connect(url):
     return sock, hostname
 
 
-def watch(callback, url=default_sock_url):
+def watch(callback, url=default_sock_url, restart_callback=None):
     """Watch docker events. Will call callback with each new event (dict).
 
         url can be either tcp://<host>:port or ipc://<path>
@@ -101,9 +102,23 @@ def watch(callback, url=default_sock_url):
 
             if len(data) < start + size + 2:
                 continue
-            payload = data[start:start+size]
-            callback(json.loads(payload))
-            buf = [data[start+size+2:]]  # Skip \r\n suffix
+            payload = data[start:start + size]
+
+            parsed_json = json.loads(payload)
+            callback(parsed_json)
+
+            if restart_callback:
+                if "status" in parsed_json:
+                    event_status = parsed_json['status']
+                else:
+                    event_status = None
+                if event_status and ("health_status: unhealthy" in event_status or "die" in event_status):
+                    print "%s seems dead or unhealthy" % parsed_json['Actor']['Attributes']["name"]
+                    print "Event status: " + event_status
+                    print "Parsed json: " + str(parsed_json)
+                    restart_callback(url, parsed_json)
+
+            buf = [data[start + size + 2:]]  # Skip \r\n suffix
 
 
 def print_callback(msg):
@@ -121,6 +136,29 @@ def prog_callback(prog, msg):
     pipe.stdin.close()
 
 
+def restart_callback(url, msg):
+    container_id = msg['id']
+    container_name = msg['Actor']['Attributes']["name"]
+    compose_service_name = msg['Actor']['Attributes']["com.docker.compose.service"]
+
+    sock, hostname = connect(url)
+    print "Sending restart request to Docker API for container: {0} ({1}), compose service name: {2}" \
+        .format(container_name, container_id, compose_service_name)
+    request = 'POST /containers/{0}/restart?t=5 HTTP/1.1\nHost: {1}\n\n'.format(container_id, hostname)
+    request = request.encode('utf-8')
+
+    with closing(sock):
+        sock.sendall(request)
+        header, payload = read_http_header(sock)
+        status, reason = header_status(header)
+
+        # checking the HTTP status, no payload should be received!
+        if status == HTTP_NO_CONTENT:
+            print "Restarting %s..." % container_name
+        else:
+            raise DockermonError('bad HTTP status: %s %s' % (status, reason))
+
+
 if __name__ == '__main__':
     from argparse import ArgumentParser
 
@@ -133,6 +171,12 @@ if __name__ == '__main__':
     parser.add_argument(
         '--version', help='print version and exit',
         action='store_true', default=False)
+
+    # restart containers on unhealthy state OR when they are dead
+    # manual kill won't restart
+    parser.add_argument('--restart-containers', dest='restart_containers', action='store_true')
+    parser.add_argument('--do-not-restart-containers', dest='restart_containers', action='store_false')
+    parser.set_defaults(restart_containers=True)
     args = parser.parse_args()
 
     if args.version:
@@ -146,6 +190,9 @@ if __name__ == '__main__':
         callback = print_callback
 
     try:
-        watch(callback, args.socket_url)
+        if args.restart_containers:
+            watch(callback, args.socket_url, restart_callback=restart_callback)
+        else:
+            watch(callback, args.socket_url)
     except (KeyboardInterrupt, EOFError):
         pass
